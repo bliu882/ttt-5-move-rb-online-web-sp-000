bb867be0c51883656e8f4ba1fdaa6710bca003a6
Brandon Liu <bliu88@live.com>
2019-02-01 16:01:43 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+user_move = gets.strip
+user_move.to_i
+
+
+
+  
 # code your input_to_index and move method here!
+=begin
+puts "Turn 2"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+position = gets.strip
+token = "x"
+board[position.to_i]=token
+=end
